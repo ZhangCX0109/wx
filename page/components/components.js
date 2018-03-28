90ef91db300fe96a9929f87026045c856dec1680
吕涛 <157179179@qq.com>
2018-03-28 14:45:23 +0800

    滑动动画效果控制

--- page/components/components.js
+++ page/components/components.js
@@ -12,38 +12,36 @@ Page({
     menuWidth: app.globalData.screenWidth * 2 / 3,
     menuLeft: -app.globalData.screenWidth * 2 / 3
   },
-  clickMe : function(e){
+  clickMe: function (e) {
     var _this = this
     var flag = this.data.menuLeft >= -10 ? -1 : 1;
-    var fps = 20;
-    var time = 600;
+    var fps = 50;
+    var time = 1200;
     var logDvalue = (eLog - 1) / fps
     var count = 0;
     var menuLeft = this.data.menuLeft
-   // console.log(flag)
+    // console.log(flag)
     console.log('')
 
-    var timer = setInterval(function (){
+      var l
+    var timer = setInterval(function () {
       ++count;
-      if(count == fps){
+      if (count == fps) {
         clearInterval(timer)
       }
-      var l = Math.log((eLog - 1) * (count / fps) + 1) 
-      l = menuLeft + _this.data.menuWidth * l * flag
-    
 
-      l = Math.pow( (Math.sin((count / fps ) * 0.5 * Math.PI) ),5) 
+      l = Math.pow((Math.cos((count / fps) * Math.PI) + 1) / 2, 4)  
+      l = 1 - l
+      var l1 = l * _this.data.menuWidth * flag + menuLeft
+  
+
       console.log(l)
-      l = l * _this.data.menuWidth * flag + menuLeft
-    //  console.log(l)
-      var newMenuLeft = menuLeft +  _this.data.menuWidth * Math.log(1 + logDvalue * count) * flag;
-      //console.log(newMenuLeft)
       _this.setData({
-        menuLeft: l
+        menuLeft: l1
       })
-    },time / fps)
+    }, time / fps)
   },
-  clickMe1 : function (e) {
+  clickMe1: function (e) {
     var _this = this
     var flag = this.data.menuLeft >= -10 ? -1 : 1;
     var fps = 20;
@@ -71,7 +69,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
@@ -83,41 +81,41 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
